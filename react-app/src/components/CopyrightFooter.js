f4210e72ed050e97351c8daa8c47c735d5d36873
harrydehix <harry.holly02@gmail.com>
2021-03-28 18:38:48 +0200

    Update ui

---
+++ react-app/src/components/CopyrightFooter.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class CopyrightFooter extends Component {
+  render() {
+    return (
+      <footer id="copyright-footer">
+        <small>&copy; Copyright 2021, Henri Hollmann</small>
+      </footer>
+    );
+  }
+}
+
+export default CopyrightFooter;
