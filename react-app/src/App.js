a4f7047d5e905e95a6ab9bfa76b73f2140541ea0
harrydehix <harry.holly02@gmail.com>
2021-03-28 17:02:11 +0200

    Add basic start page

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,24 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Tabs from "./components/Tabs";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="container" id="start-screen">
+      <h1 id="uno-heading">Uno Web</h1>
+      <h5 id="uno-subheading">AN OPEN-SOURCE GAME</h5>
+      <Tabs selected="Croc">
+        <div label="Create Lobby">
+          <form method="post">
+            <div className="form-group">
+              <label for="lname">Lobby name: </label>
+              <input
+                id="lname"
+                name="lname"
+                type="text"
+                className="form-control"
+                placeholder="e.g. henrisLobby112"
+              />
+            </div>
+            <div className="form-group">
+              <label for="lpwd">Lobby password: </label>
+              <input
+                id="lpwd"
+                name="lpwd"
+                type="password"
+                className="form-control"
+                placeholder="e.g. password212"
+              />
+              <small id="reminder" className="form-text text-muted">
+                Make sure to remember the lobbys name and password!
+              </small>
+            </div>
+
+            <button type="submit">Create Lobby</button>
+          </form>
+        </div>
+        <div label="Join Lobby">
+          <form method="post">
+            <div className="form-group">
+              <label for="lname">Lobby name: </label>
+              <input
+                id="lname"
+                name="lname"
+                type="text"
+                className="form-control"
+                placeholder="e.g. henrisLobby112"
+              />
+            </div>
+            <div className="form-group">
+              <label for="lpwd">Lobby password: </label>
+              <input
+                id="lpwd"
+                name="lpwd"
+                type="password"
+                className="form-control"
+                placeholder="e.g. password212"
+              />
+              <small id="reminder" className="form-text text-muted">
+                Have fun while playing!
+              </small>
+            </div>
+
+            <button type="submit">Join Lobby</button>
+          </form>
+        </div>
+      </Tabs>
+    </main>
   );
 }
 
